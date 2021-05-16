e40d3c7a592c7c900a86e8542dfeec7cb19b9b58
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-05-16 17:28:21 +0300

    SM-4 finalizare story vizualizare studenti

--- src/main/java/model/Instructor.java
+++ src/main/java/model/Instructor.java
@@ -6,19 +6,35 @@ import java.util.List;
 import org.dizitart.no2.objects.Id;
 
 public class Instructor {
+    private List<Student> requests;
+    private List<Student> students;
+
     @Id
     private String username;
 
     public Instructor(String username) {
         this.username = username;
         this.requests = new ArrayList<>();
+        this.students = new ArrayList<>();
     }
 
     public Instructor() {
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public List<Student> getRequests() {
@@ -28,10 +44,4 @@ public class Instructor {
     public void setRequests(List<Student> requests) {
         this.requests = requests;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    private List<Student> requests;
 }
